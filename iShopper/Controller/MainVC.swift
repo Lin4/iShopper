cb6eac0a5a3558e70c1fc7a6a6412ddf508dac50
Lingeswaran Kandasamy <lingeswarankandasamy@Lingeswarans-iMac.local>
2017-12-20 11:50:01 -0500

    calculator completed

--- iShopper/Controller/MainVC.swift
+++ iShopper/Controller/MainVC.swift
@@ -12,6 +12,9 @@ class MainVC: UIViewController {
 
     @IBOutlet weak var wagesTXT: CurrencyTextField!
     @IBOutlet weak var itemPriceTXT: CurrencyTextField!
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBOutlet weak var hoursLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
@@ -19,11 +22,30 @@ class MainVC: UIViewController {
         calcBtn.setTitle("Calculate", for: .normal)
         calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
         calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
         wagesTXT.inputAccessoryView = calcBtn
         itemPriceTXT.inputAccessoryView = calcBtn
+        
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
     }
+    
+    @IBAction func clearCalcTapped(_ sender: Any) {
+        resultLbl.isHidden = true
+        hoursLbl.isHidden = true
+        wagesTXT.text = ""
+        itemPriceTXT.text = ""
+    }
+    
     @objc func calculate() {
-        print("kkcbbfbkf")
+        if let wageTxt = wagesTXT.text, let itemPriceTXT = itemPriceTXT.text {
+            if let wage = Double(wageTxt), let price = Double(itemPriceTXT) {
+                view.endEditing(true)
+                resultLbl.isHidden = false
+                hoursLbl.isHidden = false
+                resultLbl.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+            }
+        }
     }
 }
 
